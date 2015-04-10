378aaefff84e0870e61fcd97af684184aeffb4e4
Carlo Pacioni <carlo.pacioni@gmail.com>
2015-04-10 21:14:51 +0800

    Added tests and data for tests

---
+++ tests/testthat/test-Ne_Nadults.R
@@ -0,0 +1,27 @@
+library(vortexR)
+context("test Ne_Nadults")
+
+
+test_that("test Ne", {
+  # dir
+  pac.dir <- system.file("extdata", "pacioni", package="vortexR")
+
+  # load data
+  data(pac.clas.Ne)
+  suppressMessages(NeAll <- Ne(data=pac.clas, scenarios="all", gen=2.54, yr0=50, yrt=120,
+                   save2disk=FALSE))
+
+expect_equal(pac.clas.Ne , NeAll)
+})
+
+test_that("test Nadults", {
+  # dir
+  pac.dir <- system.file("extdata", "pacioni", package="vortexR")
+
+  # load data
+  data(pac.clas.Nadults)
+  suppressMessages(NadultAll <- Nadults(data=pac.yr[[2]], scenario="all", gen=2.54, yr0=50, yrt=120,
+                       save2disk=FALSE))
+
+  expect_equal(pac.clas.Nadults, NadultAll)
+})
