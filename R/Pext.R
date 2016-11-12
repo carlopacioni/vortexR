0c5cec39caa050b5b3e78497136062ef7c03aa67
Carlo Pacioni <carlo.pacioni@gmail.com>
2016-11-12 12:09:08 +1100

    Added Pextinction and rRec

---
+++ R/Pext.R
@@ -0,0 +1,63 @@
+#' Cumulitive probability of extinction at the end of the simulation
+#'
+#' \code{Pextinct} calculates the cumulitive probability of extinction at the
+#' by calculating the proportion of runs in which a population goes extinct for
+#' each scenario.
+#'
+#' \code{Pextinct} then compares each scenario by calculating the strictly
+#' standardised mean difference (SSMD, Zhang 2007) and reports this statistic
+#' with its associated p values. Raw data are also reported.
+#'
+#' @param data The long format of run (lrun, the second element) of the output
+#' from \code{collate_run}
+#' @inheritParams pairwise
+#' @return A list with two elements, a table (\code{data.table}) with the mean
+#' Pext and its SD, the SSMD and its associated p-value for each scenario and
+#' population, and a table (\code{data.table}) for each iteration where exctinction
+#' is coded as one (and zero for no extinction)
+#' @references Zhang, X. D. 2007. A pair of new statistical parameters for quality control
+#' in RNA interference high-throughput screening assays. Genomics 89:552-561.
+#' @import data.table
+#' @export
+#' @examples
+#' # Using Pacioni et al. example data. See ?pac.run.lhs for more details.
+#' data(pac.run.lhs)
+#' Pext <- Pextinct(pac.run.lhs[[2]], project="Pacioni_et_al", scenario="ST_Classic",
+#'              ST=TRUE, save2disk=FALSE, dir_out="DataAnalysis/Pextinct")
+
+
+Pextinct <- function(data, project, scenario, ST=TRUE, save2disk=TRUE,
+                     dir_out="DataAnalysis/Pextinct") {
+    fname <- if (ST == TRUE) {
+        paste(project, "_", scenario, sep="")
+    } else {
+        project
+    }
+    data <- data.table(data)
+
+    if (ST == TRUE ) {
+        scenario <- grep("(Base)", data[, unique(Scenario)], value=TRUE)
+    }
+
+    setkey(data, YrExt)
+    data[!.(NA), Ext  := 1]
+    data[.(NA), Ext  := 0]
+    extTable <- data[, .(Pext=mean(Ext), SD=sd(Ext)), by=.(Scenario, Population)]
+    setkey(extTable, Scenario)
+    Base <- extTable[scenario, .(Pext, SD), by=Population]
+    setnames(Base, c("Pext", "SD"), c("base", "SDbase"))
+    extTable <- merge(extTable, Base, by="Population")
+    extTable[, SSMD := (base - Pext) / sqrt(SD^2 + SDbase^2)]
+    extTable[, pvalues := vortexR:::pval(SSMD)]
+
+    if (save2disk == T) {
+        # write results
+        df2disk(extTable, dir_out, fname, ".PextTable")
+        df2disk(data, dir_out, fname, ".withExt")
+    }
+    return(list(PextTable=extTable, datWithExt=data))
+}
+
+#' Calculates p-values from z-values
+#' @param x z-values
+pval <- function(x) pnorm(abs(x), lower.tail=FALSE)
---
+++ R/rRec.R
@@ -0,0 +1,76 @@
+#' Calculate the mean recovery rate (Pacioni et al in pres) and compare
+#' scenarios
+#'
+#' \code{rRec} calculates the mean and standard deviation growth rate between
+#' the time \code{yr0} and \code{yrt}, which was defined as 'recovery rate' by
+#' Pacioni et al (in press). The function then calculates the strictly
+#' sandardised mean difference (SSMD, Zhang 2007) for each scenario, and each population
+#' contained in the data. \code{rRec} uses this statistic to compare each scenario
+#' (providing associated p-values) with a baseline scenario.
+#'
+#' The means and standard deviations are calculated as:
+#' **check how to insert formula in Roxy tags**
+#' rRec = sigma(Ni*Mi) / sigma(Ni)
+#' (N1*M1+N2*M2+N3*M3)/(N1+N2+N3)
+#' SD ={N1*S1+N2*S2+N3*S3}/(N1+N2+N3)
+#'
+#' Where M is the mean growth rate in each year, N is the sample size (number
+#' of simulation runs) and S is the standard deviation.
+#'
+#' The baseline scenario is selected with the argument \code{scenario}. However,
+#' if the simulations were part of a sensitivity testing (as indicated by
+#' \code{ST}) then the baseline scenario is selected using the scenario with the
+#' suffix '(Base)'.
+#'
+#' @inheritParams pairwise
+#' @inheritParams Ne
+#' @inheritParams collate_dat
+#' @return A table (\code{data.table}) with the mean rRec and its SD, the SSMD
+#' and its associated p-value for each scenario and population
+#' @references Zhang, X. D. 2007. A pair of new statistical parameters for quality control
+#' in RNA interference high-throughput screening assays. Genomics 89:552-561.
+#'
+#' Pacioni, C., and Mayer, F. (in prep). vortexR: an R package for post Vortex
+#' simulation analysis.
+#' @import data.table
+#' @export
+#' @examples
+#' # Using Pacioni et al. example data. See ?pac.clas for more details.
+#' data(pac.clas)
+#' recov <- rRec(pac.clas, project="Pacioni_et_al", scenario="ST_Classic",
+#'               ST=TRUE, runs=3, yr0=1, yrt=120, save2disk=FALSE,
+#'               dir_out="DataAnalysis/rRec")
+
+
+rRec <- function(data, project, scenario, ST=TRUE, runs, yr0=1, yrt,
+                 save2disk=TRUE, dir_out="DataAnalysis/rRec") {
+
+  fname <- if (ST == TRUE) {
+    paste(project, "_", scenario, sep="")
+  } else {
+    project
+  }
+  data <- data.table(data)
+  if (ST == TRUE ) {
+    scenario <- grep("(Base)", data[, unique(scen.name)], value=TRUE)
+  }
+  setkey(data, Year)
+  rTable <- data[J(yr0:yrt), .(rruns=r.stoch * runs, SDruns=SD.r. * runs),
+                by=c("scen.name", "pop.name")]
+  rTable <- rTable[, .(rRec=sum(rruns) / (runs * length(yr0:yrt)),
+                     SD=sum(SDruns) / (runs * length(yr0:yrt))),
+                     by=c("scen.name", "pop.name")]
+  setnames(rTable, c("scen.name", "pop.name"), c("Scenario", "Population"))
+  setkey(rTable, Scenario)
+  Base <- rTable[scenario, .(rRec, SD), by=Population]
+  setnames(Base, c("rRec", "SD"), c("base", "SDbase"))
+  rTable <- merge(rTable, Base, by="Population")
+  rTable[, SSMD := (rRec - base) / sqrt(SD^2 + SDbase^2)]
+  rTable[, pvalues := vortexR:::pval(SSMD)]
+
+  if (save2disk == T) {
+    # write results
+    df2disk(rTable, dir_out, fname, ".rTable")
+  }
+  return(rTable)
+}
