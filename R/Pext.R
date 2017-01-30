a16cf74eab5894c7cd2c98f9cac94a5917fa70f7
Carlo Pacioni <carlo.pacioni@gmail.com>
2017-01-31 09:26:58 +1100

    pval: separate file

--- R/Pext.R
+++ R/Pext.R
@@ -59,15 +59,4 @@ Pextinct <- function(data, project, scenario, ST=FALSE, save2disk=TRUE,
     return(list(PextTable=extTable, datWithExt=data))
 }
 
-#' Calculates p-values from z-values
-#'
-#' \code{pval} calculates one-tailed p values from a vector that contains
-#'   z-values and it is geenrally used internally.
-#'
-#' @param x z-values
-#' @return A numeric vector of length equal to length(x)
-#' @export
-#' @examples
-#'  z <- c(1.645, 1.96, 3.09)
-#'  pval(z)
-pval <- function(x) pnorm(abs(x), lower.tail=FALSE)
+
